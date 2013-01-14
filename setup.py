829a7a642196c5ffa79b1eefa86b7ff5c575bb3b
Joshua Fialkoff <joshuafialkoff@justice.westell.com>
2013-01-14 17:38:19 -0500

    Added setup script and some of the initial structure.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import sys
+
+from setuptools import setup, find_packages
+
+from bigfoot import __version__
+
+install_requires = [
+    'django >= 1.4',
+    'django-tables2 == 0.13'
+]
+
+setup(
+    name = "django-bigfoot",
+    version = __version__,
+    description = "A Django library for defining HTML building blocks (e.g.," +
+        " forms, form fields, tables) that can be easily rendered into HTML.",
+    url = "https://github.com/setaris/django-bigfoot",
+    author = "Setaris",
+    author_email = "support@setaris.com",
+    packages = find_packages(),
+    install_requires = install_requires,
+)
