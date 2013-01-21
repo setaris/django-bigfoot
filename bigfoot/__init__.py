b88080d4d7770be3c523b84ee4d9d1cb36195364
Joshua Fialkoff <joshuafialkoff@justice.westell.com>
2013-01-21 18:56:35 -0500

    Fixed installation error.

--- bigfoot/__init__.py
+++ bigfoot/__init__.py
@@ -1,2 +1,7 @@
-from .elements import *
 __version__ = "0.1"
+try:
+    from .elements import *
+except ImportError:
+    # Can't find project's settings file - this happens during installation.
+    # Not sure if there's a better way to solve this.
+    pass
